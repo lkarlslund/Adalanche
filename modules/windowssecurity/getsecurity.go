178709aa598684803a4e7114c49c24c6774ce7e9
Lars Karlslund <lars@karlslund.dk>
2021-09-13 15:58:05 +0200

    Some sanity checking when copying memory based ACL, and now actually copies the right memory area

--- modules/windowssecurity/getsecurity.go
+++ modules/windowssecurity/getsecurity.go
@@ -1,6 +1,7 @@
 package windowssecurity
 
 import (
+	"errors"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -15,12 +16,27 @@ func GetOwnerAndDACL(objectName string, objectType windows.SE_OBJECT_TYPE) (*win
 		if err == nil {
 			nativedacl, _, err := sd.DACL()
 			if err == nil {
-				// Absolutely horrendous, but I don't see any other way - see windows.DACL definition
-				dacllength := (*[2]uint16)(unsafe.Pointer(&nativedacl))[1]
-				if dacllength != 0 {
-					// We'd have crashed already
-					dacl = make([]byte, dacllength)
-					copy(dacl, (*[0x7fff0000]byte)(unsafe.Pointer(&dacl))[:dacllength:dacllength])
+				revision := (*[8]uint8)(unsafe.Pointer(nativedacl))[0]
+				if revision != 1 && revision != 2 {
+					// Wrong version
+					err = errors.New("Only DACL version 1 is supported")
+				}
+				if (*[8]uint8)(unsafe.Pointer(nativedacl))[1] != 0 {
+					// Sbz1 should be zero
+					err = errors.New("Sbz1 is nonzero")
+				}
+				if (*[8]uint16)(unsafe.Pointer(nativedacl))[3] != 0 {
+					// Sbz2 should also be zero
+					err = errors.New("Sbz2 is nonzero")
+				}
+				if err == nil {
+					// Absolutely horrendous, but I don't see any other way - see windows.DACL definition
+					dacllength := (*[2]uint16)(unsafe.Pointer(nativedacl))[1]
+					if dacllength != 0 {
+						// We'd have crashed already
+						dacl = make([]byte, dacllength)
+						copy(dacl, (*[0x7fff0000]byte)(unsafe.Pointer(nativedacl))[:dacllength:dacllength])
+					}
 				}
 			}
 		}
