97895520fe73545eb349bdb8ee85ed91e967fd88
Lars Karlslund <lars@karlslund.dk>
2021-11-01 12:38:22 +0100

    Embedding build version and date fix

--- build.cmd
+++ build.cmd
@@ -12,7 +12,7 @@ for /F "usebackq delims=" %%a in (`git describe --tags --exclude latest`) do (
      set VERSION=%%a
 )
 
-set LDFLAGS=-X version.Program=adalanche -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%VERSION%
+set LDFLAGS=-X github.com/lkarlslund/adalanche/modules/version.Program=adalanche -X github.com/lkarlslund/adalanche/modules/version.Builddate=%BUILDDATE% -X github.com/lkarlslund/adalanche/modules/version.Commit=%COMMIT% -X github.com/lkarlslund/adalanche/modules/version.Version=%VERSION%
 set GOARCH=amd64
 set GOOS=windows
 go build -ldflags "%LDFLAGS%" -o adalanche-windows-x64-%VERSION%.exe ./adalanche
@@ -21,7 +21,7 @@ go build -ldflags "%LDFLAGS%" -o adalanche-osx-x64-%VERSION% ./adalanche
 set GOOS=linux
 go build -ldflags "%LDFLAGS%" -o adalanche-linux-x64-%VERSION% ./adalanche
 
-set LDFLAGS=-X version.Program=adalanche-collector -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%VERSION%
+set LDFLAGS=-X github.com/lkarlslund/adalanche/modules/version.Program=adalanche-collector -X github.com/lkarlslund/adalanche/modules/version.Builddate=%BUILDDATE% -X github.com/lkarlslund/adalanche/modules/version.Commit=%COMMIT% -X github.com/lkarlslund/adalanche/modules/version.Version=%VERSION%
 set GOARCH=386
 set GOOS=windows
 go build -ldflags "%LDFLAGS%" -o adalanche-collector-windows-386-%VERSION%.exe ./collector
--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ VERSION=`git describe --tags --exclude latest`
 
 EXIT_STATUS=0
 
-LDFLAGS="-X version.Program=adalanche -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$VERSION"
+LDFLAGS="-X github.com/lkarlslund/adalanche/modules/version.Program=adalanche -X github.com/lkarlslund/adalanche/modules/version.Builddate=$BUILDDATE -X github.com/lkarlslund/adalanche/modules/version.Commit=$COMMIT -X github.com/lkarlslund/adalanche/modules/version.Version=$VERSION"
 
 GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-windows-x64-$VERSION.exe ./adalanche || EXIT_STATUS=$?
 GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-osx-x64-$VERSION ./adalanche || EXIT_STATUS=$?
 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-linux-x64-$VERSION ./adalanche || EXIT_STATUS=$?
 
-LDFLAGS="-X version.Program=adalanche-collector -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$VERSION"
+LDFLAGS="-X github.com/lkarlslund/adalanche/modules/version.Program=adalanche-collector -X github.com/lkarlslund/adalanche/modules/version.Builddate=$BUILDDATE -X github.com/lkarlslund/adalanche/modules/version.Commit=$COMMIT -X github.com/lkarlslund/adalanche/modules/version.Version=$VERSION"
 
 GOOS=windows GOARCH=386 go build -ldflags "$LDFLAGS" -o adalanche-collector-windows-386-$VERSION.exe ./collector || EXIT_STATUS=$?
 
