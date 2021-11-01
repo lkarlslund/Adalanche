1f8946bb0e5c8a1bc8f13e5bd328c6b10060be48
Lars Karlslund <lars@karlslund.dk>
2021-11-01 10:51:33 +0100

    Renamed version variables in scripts

--- build.cmd
+++ build.cmd
@@ -9,19 +9,19 @@ for /F "usebackq delims=" %%a in (`git rev-parse --short HEAD`) do (
 
 for /F "usebackq delims=" %%a in (`git describe --tags --exclude latest`) do (
      ENDLOCAL
-     set GIT_VERSION=%%a
+     set VERSION=%%a
 )
 
-set LDFLAGS=-X version.Program=adalanche -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%GIT_VERSION%
+set LDFLAGS=-X version.Program=adalanche -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%VERSION%
 set GOARCH=amd64
 set GOOS=windows
-go build -ldflags "%LDFLAGS%" -o adalanche-windows-x64-%GIT_VERSION%.exe ./adalanche
+go build -ldflags "%LDFLAGS%" -o adalanche-windows-x64-%VERSION%.exe ./adalanche
 set GOOS=darwin
-go build -ldflags "%LDFLAGS%" -o adalanche-osx-x64-%GIT_VERSION% ./adalanche
+go build -ldflags "%LDFLAGS%" -o adalanche-osx-x64-%VERSION% ./adalanche
 set GOOS=linux
-go build -ldflags "%LDFLAGS%" -o adalanche-linux-x64-%GIT_VERSION% ./adalanche
+go build -ldflags "%LDFLAGS%" -o adalanche-linux-x64-%VERSION% ./adalanche
 
-set LDFLAGS=-X version.Program=adalanche-collector -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%GIT_VERSION%
+set LDFLAGS=-X version.Program=adalanche-collector -X version.Builddate=%BUILDDATE% -X version.Commit=%COMMIT% -X version.Version=%VERSION%
 set GOARCH=386
 set GOOS=windows
-go build -ldflags "%LDFLAGS%" -o adalanche-collector-windows-386-%GIT_VERSION%.exe ./collector
+go build -ldflags "%LDFLAGS%" -o adalanche-collector-windows-386-%VERSION%.exe ./collector
--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 BUILDDATE=`date +%Y%m%d`
 COMMIT=`git rev-parse --short HEAD`
-GIT_VERSION=`git describe --tags --exclude latest`
+VERSION=`git describe --tags --exclude latest`
 
 EXIT_STATUS=0
 
-LDFLAGS="-X version.Program=adalanche -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$GIT_VERSION"
+LDFLAGS="-X version.Program=adalanche -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$VERSION"
 
-GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-windows-x64-$GIT_VERSION.exe ./adalanche || EXIT_STATUS=$?
-GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-osx-x64-$GIT_VERSION ./adalanche || EXIT_STATUS=$?
-GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-linux-x64-$GIT_VERSION ./adalanche || EXIT_STATUS=$?
+GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-windows-x64-$VERSION.exe ./adalanche || EXIT_STATUS=$?
+GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-osx-x64-$VERSION ./adalanche || EXIT_STATUS=$?
+GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o adalanche-linux-x64-$VERSION ./adalanche || EXIT_STATUS=$?
 
-LDFLAGS="-X version.Program=adalanche-collector -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$GIT_VERSION"
+LDFLAGS="-X version.Program=adalanche-collector -X version.Builddate=$BUILDDATE -X version.Commit=$COMMIT -X version.Version=$VERSION"
 
-GOOS=windows GOARCH=386 go build -ldflags "$LDFLAGS" -o adalanche-collector-windows-386-$GIT_VERSION.exe ./collector || EXIT_STATUS=$?
+GOOS=windows GOARCH=386 go build -ldflags "$LDFLAGS" -o adalanche-collector-windows-386-$VERSION.exe ./collector || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
