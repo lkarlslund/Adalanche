69eadcff28b32b645b0bd6a9e5c9ab1a6bd123d9
Lars Karlslund <lars@karlslund.dk>
2025-10-20 17:08:25 +0200

    Only set Merge flag on attribute that reference node attributes not defined directly in the OpenGraph file

--- modules/integrations/opengraph/analyze/process.go
+++ modules/integrations/opengraph/analyze/process.go
@@ -24,18 +24,19 @@ func processOpenGraphData(g *engine.IndexedGraph, ogd opengraph.Model) error {
 	seenMatchAttrs := make(map[engine.Attribute]struct{})
 	for _, edge := range ogd.Graph.Edges {
 		startAttr := engine.NewAttribute(edge.Start.MatchBy)
-		if _, seen := seenMatchAttrs[startAttr]; !seen {
+		endAttr := engine.NewAttribute(edge.End.MatchBy)
+
+		startNode, startFound := g.FindOrAdd(startAttr, engine.NV(edge.Start.MatchBy))
+		endNode, endFound := g.FindOrAdd(endAttr, engine.NV(edge.End.MatchBy))
+
+		if _, seen := seenMatchAttrs[startAttr]; !seen && !startFound {
 			startAttr.Flag(engine.Merge)
 			seenMatchAttrs[startAttr] = struct{}{}
 		}
-		endAttr := engine.NewAttribute(edge.End.MatchBy).Flag(engine.Merge)
-		if _, seen := seenMatchAttrs[endAttr]; !seen {
+		if _, seen := seenMatchAttrs[endAttr]; !seen && !endFound {
 			endAttr.Flag(engine.Merge)
 			seenMatchAttrs[endAttr] = struct{}{}
 		}
-
-		startNode, _ := g.FindOrAdd(startAttr, engine.NV(edge.Start.MatchBy))
-		endNode, _ := g.FindOrAdd(endAttr, engine.NV(edge.End.MatchBy))
 
 		edge := engine.NewEdge(edge.Kind)
 		g.EdgeTo(startNode, endNode, edge)
